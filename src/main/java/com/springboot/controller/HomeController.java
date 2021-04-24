5ad84cc20c145af014f9d3c286d9cb308190af50
Sahil Bhatia <sahilofficial671@gmail.com>
2021-04-24 15:27:06 +0530

    Initial Commit :rocket:

---
+++ src/main/java/com/springboot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.springboot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping(value = {"/",  "/hello"})
+	public ResponseEntity<String> greet(){
+		return new ResponseEntity<String>("My first spring boot", HttpStatus.OK);	
+	}
+}
